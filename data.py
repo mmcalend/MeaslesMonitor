b740acb11f62f19382927bcff9438ca707dadafa
mmcalend <46433511+mmcalend@users.noreply.github.com>
2025-08-11 11:02:17 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,15 +1,39 @@
 import pandas as pd
 
 def load_all_data():
-    df = pd.read_csv("https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/USMeaslesCases.csv", parse_dates=["mmwr_week_start"])
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/USMeaslesCases.csv",
+        parse_dates=["mmwr_week_start"]
+    )
     try:
-        df19 = pd.read_csv("https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/NNDSSMeasles2019.csv", encoding="utf-8")
+        df19 = pd.read_csv(
+            "https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/NNDSSMeasles2019.csv",
+            encoding="utf-8"
+        )
     except UnicodeDecodeError:
-        df19 = pd.read_csv("https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/NNDSSMeasles2019.csv", encoding="latin1")
+        df19 = pd.read_csv(
+            "https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/NNDSSMeasles2019.csv",
+            encoding="latin1"
+        )
 
-    dfdetails = pd.read_csv("https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/USMeaslesCasesDetails.csv")
-    mmr = pd.read_csv("https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/MMR.csv")
-    df_schools = pd.read_csv("https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/24-25ADHSMMRKCoverage.csv")
-    return df, df19, dfdetails, mmr, df_schools
+    dfdetails = pd.read_csv(
+        "https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/USMeaslesCasesDetails.csv"
+    )
+    mmr = pd.read_csv(
+        "https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/MMR.csv"
+    )
+    df_schools = pd.read_csv(
+        "https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/24-25ADHSMMRKCoverage.csv"
+    )
+
+    try:
+        df_us_cdc = pd.read_csv(
+            "https://raw.githubusercontent.com/mmcalend/USMeaslesData/refs/heads/main/CDC_US_WeeklyRashOnset.csv",
+            parse_dates=["mmwr_week_start"]
+        )
+    except Exception:
+        df_us_cdc = pd.DataFrame(columns=["mmwr_week_start", "cases"])
+
+    return df, df19, dfdetails, mmr, df_schools, df_us_cdc
 
 
